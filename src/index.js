f2a135f74b55546adb7592b37566ba58b058d96b
Grete <greteveetla@gmail.com>
2022-03-23 14:35:33 +0200

    starting point

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+
+const Game = () => {
+  return (
+    <div>
+      Game
+    </div>
+  );
+};
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);

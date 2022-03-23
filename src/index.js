c1b79bb2cab5c4297d5bff456bae440625649199
Grete <greteveetla@gmail.com>
2022-03-23 17:54:18 +0200

    component state

--- src/index.js
+++ src/index.js
@@ -8,17 +8,24 @@ const Square = (props) => {
 
   return (
     <button className='square'
-      onClick={() => setValue('X')}
+      onClick={() => { }}
     >
-      {value}
+      {props.value}
     </button>
   );
 };
 
 const Board = () => {
+  const initialSquares = [
+    null, null, null,
+    null, null, null,
+    null, null, null
+  ];
+  const [squares, setSquares] = useState(initialSquares);
+
   const renderSquare = (i) => {
     return (
-      <Square />
+      <Square value={squares[i]} />
     );
   };
 
